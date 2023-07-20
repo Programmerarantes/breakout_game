888a4e3426758f801452e90e793666e7c61954dd
Matheus Arantes <125316650+Programmerarantes@users.noreply.github.com>
2023-07-20 19:00:47 -0300

    criando a estrutura logica do jogo em js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const grid = document.querySelector('.grid')
 const blockWidth = 100
 const blockHeight = 200
+const boardWidth = 560
+const boardHeight = 300
+const ballDiameter = 20
+let timerId
+let xDirection = 2
+let yDirection = 2 
+
+const userStart = [230, 10]
+let currentPosition = userStart
+
+
+const ballStart = [270, 35]
+let ballCurrentPosition = ballStart
 
 //create Block
 class Block {
@@ -45,3 +58,75 @@ function addBlocks(){
 }
 
 addBlocks()
+
+//add user
+const user = document.createElement('div')
+user.classList.add('user')
+drawUser()
+grid.appendChild(user)
+
+//draw the user 
+function drawUser() {
+    user.style.left = currentPosition[0] + 'px'
+    user.style.bottom = currentPosition[1] + 'px'
+}
+
+//draw the ball
+function drawBall() {
+    ball.style.left = ballCurrentPosition[0] + 'px'
+    ball.style.bottom = ballCurrentPosition[1] + 'px'
+}
+
+//move user
+function moveUser(e) {
+    switch(e.key) {
+        case 'ArrowLeft':
+            if(currentPosition[0] > 0) {
+             currentPosition[0] -= 10
+            drawUser()
+            }
+            break;
+        case 'ArrowRight': 
+            if(currentPosition[0] < boardWidth - blockWidth) {
+            currentPosition[0] += 10
+            drawUser()   
+            }
+            break;
+    }
+}
+document.addEventListener('keydown', moveUser)
+
+
+//add ball
+const ball = document.createElement('div')
+ball.classList.add('ball')
+drawBall()
+grid.appendChild(ball)
+
+//move the ball
+function moveBall() {
+    ballCurrentPosition[0] += xDirection
+    ballCurrentPosition[1] += yDirection
+    drawBall()
+    checkForCollisions()    
+}
+
+timerId = setInterval(moveBall, 30)
+
+//check for colission
+
+function checkForCollisions() {
+    //check to not go over the size of the div
+    if(ballCurrentPosition[0] >= (boardWidth - ballDiameter) || 
+        ballCurrentPosition[1] >= (boardHeight - ballDiameter)
+    ) {
+        changeDirection()
+    }
+}
+
+function changeDirection() {
+    if (xDirection === 2 && yDirection === 2) {
+        xDirection = -2
+        return
+    }
+}
